9481261ff17d3499454011098af58e7352cacccd
Ryan <ryan.piinheiro@hotmail.com>
2024-10-10 19:31:55 -0300

    styles and routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default';
+import { BrowserRouter } from 'react-router-dom';
+
+import { GlobalStyle } from './styles/global';
+import { Router } from './Router';
+
 export function App() {
 
   return (
-    <h1>hello world</h1>
+    <ThemeProvider theme={defaultTheme}>  
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
 
